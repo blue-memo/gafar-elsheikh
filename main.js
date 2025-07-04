60c36a5d99576fed9163cc1191ab42501e3e1c93
Mohammed Waleed <130412952+blue-memo@users.noreply.github.com>
2025-07-04 14:41:49 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 const checkbox = document.querySelector('.theme-switch__checkbox');
+
+if (localStorage.getItem('theme') === 'dark') {
+  document.body.classList.add('dark-mode');
+  checkbox.checked = true;
+}
+
 checkbox.addEventListener('change', () => {
   document.body.classList.toggle('dark-mode', checkbox.checked);
+  if (checkbox.checked) {
+    localStorage.setItem('theme', 'dark');
+  } else {
+    localStorage.setItem('theme', 'light');
+  }
 });
 
 const hamburger = document.getElementById("hamburger");
